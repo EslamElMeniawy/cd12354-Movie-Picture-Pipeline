8aec3dcedadc4813eb988e53bf6e02ed86446bb3
Eslam El-Meniawy <eelmeniawy@ejada.com>
2025-04-08 10:36:22 +0200

    chore: setup `CD`

--- setup/init.sh
+++ setup/init.sh
@@ -5,8 +5,19 @@ echo "Fetching IAM github-action-user ARN"
 userarn=$(aws iam get-user --user-name github-action-user | jq -r .User.Arn)
 
 # Download tool for manipulating aws-auth
+ARCH=$(uname -m)
+OS=$(uname | tr '[:upper:]' '[:lower:]')
+
+case $ARCH in
+  x86_64) ARCH=amd64 ;;
+  aarch64 | arm64) ARCH=arm64 ;;
+  *) echo "Unsupported architecture: $ARCH" && exit 1 ;;
+esac
+
+FILENAME="aws-iam-authenticator_0.6.2_${OS}_${ARCH}"
+URL="https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/${FILENAME}"
 echo "Downloading tool..."
-curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_linux_amd64 -o aws-iam-authenticator
+curl -L "$URL" -o aws-iam-authenticator
 chmod +x aws-iam-authenticator
 
 echo "Updating permissions"
